fc27647c2f689be74625f156535cecfc8f45c2cb
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-03-27 02:01:47 +0530

    Update ecs_service.tf

--- terra-ecs/ecs_service.tf
+++ terra-ecs/ecs_service.tf
@@ -19,8 +19,8 @@ resource "aws_security_group" "express_security_group" {
     vpc_id= "vpc-0bf168604dc074cc9"
 
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = 3000
+        to_port = 3000
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
