9ef3c02285f405e503a48ef25bd122c180ce16e7
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-03-27 00:13:00 +0530

    Update var.tf

--- terra-ecs/var.tf
+++ terra-ecs/var.tf
@@ -1,1 +1,20 @@
+variable "REPO_NAME" {
+  type        = string
+  default = ""
+}
+
+variable "TAG" {
+  type        = string
+  default = ""
+}
+
+variable "AWS_ACCESS_KEY" {
+  type        = string
+  default = ""
+}
+
+variable "AWS_SECRET_KEY" {
+  type        = string
+  default = ""
+}
 
