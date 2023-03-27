a3db663db6f19d6b1596d18b88538a3d5eb499e9
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-03-27 22:49:19 +0530

    Update main.tf

--- terra-ecs/main.tf
+++ terra-ecs/main.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "cc-tf-state-backend-ci-cd"
         key = "tf-infra/terraform.tfstate"
-        region = "ap-south-1"
+        region = "us-east-1"
         dynamodb_table = "terraform-state-locking"
         encrypt = true
     }
