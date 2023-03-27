506cca62612570dd25693e8db26e013d83246759
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-03-27 23:44:09 +0530

    Update tf-state.tf

--- terra-ecs/modules/tf-state/tf-state.tf
+++ terra-ecs/modules/tf-state/tf-state.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 }
 
 # Dynamo DB Table for Locking TF Config
-resource "aws_dyanamodb_table" "terraform_locks" {
+resource "aws_dynamodb_table" "terraform_locks" {
   name = "terraform-state-locking2"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
